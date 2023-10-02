e5fc05c1a0de5e7cf078b50477575c758b90eb23
Documenter.jl <documenter@juliadocs.github.io>
2023-10-02 15:25:59 +0000

    build based on 14c6456

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VirialUEG","category":"page"},{"location":"#VirialUEG","page":"Home","title":"VirialUEG","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VirialUEG.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VirialUEG]","category":"page"}]
+}
